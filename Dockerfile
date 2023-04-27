004bb3ad5a3673ebf71a6bfc0118651d6c76c36b
Jakes Lee <jakeslee66@gmail.com>
2023-04-27 21:49:18 +0800

    support multiplatform image building

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,15 @@
-FROM golang:1.20.3 as builder
 
-ADD . /go/src/exporter
 
-WORKDIR /go/src/exporter
-
-# 禁用 CGO，避免 so 问题
-ENV CGO_ENABLED=0
-RUN flags="-X main.buildTime=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.version=`git describe --long --dirty --abbrev=6 --tags`" &&\
-    go build -ldflags "$flags" -v -o /go/src/bin/exporter main.go
-
-FROM ccr.ccs.tencentyun.com/imoe-tech/base-image:alpine-3.14.0-tz
+FROM jakes/base-image:alpine-3.14.0-tz
 LABEL maintainers="Jakes Lee"
 LABEL description="iKuai exporter"
+ARG TARGETPLATFORM
+
+ADD ./output /output
+RUN cp /output/"$TARGETPLATFORM"/app /app
 
 EXPOSE 9090
-
-COPY --from=builder /go/src/bin/exporter /exporter
-
 WORKDIR /data
 
-RUN chmod +x /exporter
-CMD ["/exporter"]
+RUN chmod +x /app
+CMD ["/app"]
--- build.sh
+++ build.sh
@@ -1,6 +1,27 @@
-#/bin/sh
+#!/bin/sh
 
-VERSION=`git describe --long --dirty --abbrev=6 --tags`
+version=$(git describe --long --dirty --abbrev=6 --tags)
+flags="-X main.buildTime=$(date -u '+%Y-%m-%d_%I:%M:%S%p') -X main.version=$version"
 
-docker build -t ccr.ccs.tencentyun.com/imoe-tech/go-playground:ikuai-exporter-$VERSION . &&\
-docker push ccr.ccs.tencentyun.com/imoe-tech/go-playground:ikuai-exporter-$VERSION
+export CGO_ENABLED=0 GOOS=linux
+
+echo "start to build version:$version"
+
+mkdir -p ./output/linux/
+
+for i in "arm64" "amd64" ; do
+  echo "building for $i..."
+  GOARCH="$i" go build -ldflags "$flags" -o ./output/linux/$i/app main.go
+  chmod +x ./output/linux/$i/app
+done
+
+image=ccr.ccs.tencentyun.com/imoe-tech/go-playground:ikuai-exporter-"$version"
+echo "packaging docker multiplatform image: $image"
+
+docker buildx build --push \
+  --platform linux/amd64,linux/arm64 \
+  --build-arg VERSION="$version" \
+  -t "$image" .
+
+echo "finished: $image"
+
